232e64c482a13c7299ebfda6f48d051b66a26958
lawphotog <laurence.nyein@gmail.com>
2014-10-24 15:53:48 +0200

    inital commit

---
+++ www/js/app.js
@@ -0,0 +1,161 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', [
+  'ionic',
+  'starter.controllers',
+  'auth0',
+  'angular-storage',
+  'angular-jwt',
+  'geolocation'
+])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, authProvider, $httpProvider,
+                 jwtInterceptorProvider) {
+
+  $stateProvider
+
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
+    })
+
+      .state('app.nearyou', {
+        url: '/nearyou',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/nearyou.html',
+            controller: 'NearYouCtrl'
+          }
+        },
+        data: {
+          // This tells Auth0 that this state requires the user to be logged in.
+          // If the user isn't logged in and he tries to access this state
+          // he'll be redirected to the login page
+          requiresLogin: true
+        }
+      })
+
+      .state('login', {
+        url: '/login',
+        views: {
+          'menuContent' :{
+            templateUrl: "templates/login.html",
+            controller: 'LoginCtrl'
+          }
+        }
+      })
+    // Your app states
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'templates/dashboard.html',
+        data: {
+          // This tells Auth0 that this state requires the user to be logged in.
+          // If the user isn't logged in and he tries to access this state
+          // he'll be redirected to the login page
+          requiresLogin: true
+        }
+      })
+
+      //.state('app.signup', {
+      //  url: '/signup',
+      //  views: {
+      //    'menuContent': {
+      //      templateUrl: 'templates/signup.html',
+      //      controller: 'SignUpCtrl'
+      //    }
+      //  }
+      //})
+
+    .state('app.search', {
+      url: "/search",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/search.html"
+        }
+      }
+    })
+
+    .state('app.categories', {
+      url: "/categories",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/categories.html",
+          controller: 'CategoriesCtrl'
+        }
+      }
+    })
+
+    .state('app.facilities', {
+      url: "/facilities/:categoryId",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/facilities.html",
+          controller: 'FacilitiesCtrl'
+        }
+      }
+    })
+
+      .state('app.facility', {
+        url:'/facility/:id',
+        views: {
+          'menuContent' : {
+            templateUrl: 'templates/facility.html',
+            controller: 'FacilityCtrl'
+          }
+        }
+
+      })
+
+  ;
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/categories');
+
+      authProvider.init({
+        domain: 'nghackathon.auth0.com',
+        clientID: 'GFXtzOcaPmBPZnHrKuFa3qdgMLGizm0J',
+        loginState: 'login'
+      });
+})
+
+.run(function(auth) {
+  // This hooks al auth events to check everything as soon as the app starts
+  auth.hookEvents();
+})
+
+
+.factory('dataService', function($http) {
+
+      function get_categories(){
+
+      }
+
+
+      return {
+        get_categories: get_categories,
+        get_equipements: get_equipements,
+        get_equipement: get_equipement
+      }
+
+
+});
+
---
+++ www/js/controllers.js
@@ -0,0 +1,272 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, store, $location, auth) {
+  //// Form data for the login modal
+  //$scope.loginData = {};
+  //
+  //// Create the login modal that we will use later
+  //$ionicModal.fromTemplateUrl('templates/login.html', {
+  //  scope: $scope
+  //}).then(function(modal) {
+  //  $scope.modal = modal;
+  //});
+  //
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+    //$scope.logout = function() {
+    //    auth.signout();
+    //    store.remove('profile');
+    //    store.remove('token');
+    //};
+
+  // Open the login modal
+  $scope.login = function() {
+      auth.signin({
+          authParams: {
+              scope: 'openid offline_access',
+              device: 'Mobile device'
+          }
+      }, function(profile, token, accessToken, state, refreshToken) {
+          // Success callback
+          store.set('profile', profile);
+          store.set('token', token);
+          store.set('refreshToken', refreshToken);
+          $location.path('/');
+
+          console.log('success login');
+          console.log(profile.nickname);
+
+      }, function() {
+          // Error callback
+      });
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+
+      auth.signin({
+          authParams: {
+              scope: 'openid offline_access',
+              device: 'Mobile device'
+          }
+      }, function(profile, token, accessToken, state, refreshToken) {
+          // Success callback
+          store.set('profile', profile);
+          store.set('token', token);
+          store.set('refreshToken', refreshToken);
+          $location.path('/');
+      }, function() {
+          // Error callback
+      });
+
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+
+  //$scope.signupData = {};
+  //
+  //$ionicModal.fromTemplateUrl('templates/signup.html', {
+  //  scope: $scope
+  //}).then(function(modal){
+  //  $scope.signupmodal = modal;
+  //});
+  //
+  //    // Triggered in the login modal to close it
+  //$scope.closeSignup = function() {
+  //  $scope.signupmodal.hide();
+  //};
+  //
+  //// Open the login modal
+  //$scope.signup = function() {
+  //  $scope.signupmodal.show();
+  //};
+  //
+  //// Perform the login action when the user submits the login form
+  //$scope.doSignup = function() {
+  //  console.log('Doing Signup', $scope.signupData);
+  //
+  //  // Simulate a login delay. Remove this and replace with your login
+  //  // code if using a login system
+  //  $timeout(function() {
+  //    $scope.closeSignup();
+  //  }, 1000);
+  //};
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('CategoriesCtrl', function($scope, $http, $ionicLoading) {
+
+  $scope.categories = [];
+
+  var query = 'https://api.paris.fr/api/data/1.0/Equipements/get_categories/?token=59b44ccd1735f4d11b1de27eff03107e174a2564f32979f663ed5d6e80287e5e';
+
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
+
+  $http.get(query)
+      .success(function(res){
+          angular.copy(res.data, $scope.categories);
+          $ionicLoading.hide();
+      })
+      .error(function(){
+        console.log('error');
+        $ionicLoading.hide();
+      });
+})
+
+.controller('FacilitiesCtrl', function($scope, $http, $stateParams, $ionicLoading){
+
+      var categoryId = $stateParams.categoryId;
+
+      $scope.facilities = [];
+  var query = 'https://api.paris.fr/api/data/1.0/Equipements/get_equipements/?token=59b44ccd1735f4d11b1de27eff03107e174a2564f32979f663ed5d6e80287e5e&cid=' + categoryId + '&offset=0&limit=10';
+
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
+
+      $http.get(query)
+          .success(function (res) {
+            angular.copy(res.data, $scope.facilities);
+            $ionicLoading.hide();
+          })
+          .error(function(){
+            console.log('error');
+            $ionicLoading.hide();
+          });
+
+})
+
+.controller('FacilityCtrl', function($scope, $http, $stateParams, $ionicLoading) {
+
+      var id = $stateParams.id;
+
+      $scope.facility = [];
+
+      var query = 'https://api.paris.fr/api/data/1.0/Equipements/get_equipement/?token=59b44ccd1735f4d11b1de27eff03107e174a2564f32979f663ed5d6e80287e5e&id=' + id;
+
+      $ionicLoading.show({
+        template: 'Loading...'
+      });
+
+      $http.get(query)
+          .success(function(res) {
+
+            $scope.facility = res.data[0];
+            $ionicLoading.hide();
+          })
+          .error(function(){
+            console.log('error');
+            $ionicLoading.hide();
+          });
+
+    })
+  .controller('SignUpCtrl', function($scope, $http){
+
+    $scope.doSignup = function(){
+      console.log('inside sideup controller');
+    }
+
+  })
+
+.controller('NearYouCtrl', function($scope, $http, geolocation, $ionicLoading) {
+
+        $scope.categories = [];
+
+        var query = 'https://api.paris.fr/api/data/1.0/Equipements/get_categories/?token=59b44ccd1735f4d11b1de27eff03107e174a2564f32979f663ed5d6e80287e5e';
+
+        $ionicLoading.show({
+            template: 'Loading...'
+        });
+
+        $http.get(query)
+            .success(function(res){
+                angular.copy(res.data, $scope.categories);
+                $ionicLoading.hide();
+            })
+            .error(function(){
+                console.log('error');
+                $ionicLoading.hide();
+        });
+
+        $scope.facilities = [];
+
+        $scope.GetFacilities = function(category){
+
+            geolocation.getLocation().then(function(data) {
+
+                if(data){
+
+                    var url = 'https://api.paris.fr/api/data/1.0/Equipements/get_geo_equipements/?token=59b44ccd1735f4d11b1de27eff03107e174a2564f32979f663ed5d6e80287e5e';
+                    var query = '&cid=' + category + '&offset=0&limit=10&lat=' + data.coords.latitude + '&lon=' + data.coords.longitude;
+
+                    $http.get(url + query)
+                        .success(function (res) {
+                            console.log($scope.facilities);
+                            angular.copy(res.data, $scope.facilities)
+                        })
+                        .error(function(){
+                            console.log('error');
+                        });
+                }
+            }, function(){
+                console.log('error getting location');
+            });
+        }
+
+
+
+
+
+
+    })
+
+//.controller('LoginCtrl', function($scope, store, $location, auth) {
+//        $scope.login = function() {
+//
+//
+//
+//            auth.signin({
+//                authParams: {
+//                    scope: 'openid offline_access',
+//                    device: 'Mobile device'
+//                }
+//            }, function(profile, token, accessToken, state, refreshToken) {
+//                // Success callback
+//                store.set('profile', profile);
+//                store.set('token', token);
+//                store.set('refreshToken', refreshToken);
+//                $location.path('/');
+//
+//                console.log('success login');
+//                console.log(profile.nickname);
+//
+//            }, function() {
+//                // Error callback
+//            });
+//        }
+//    })
+
+;
